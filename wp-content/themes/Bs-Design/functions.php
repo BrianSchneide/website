3aee5b2178a828d915499aaff0e72b61aeb18c5d
Brian <brian.schneide@maxxpotential.com>
2018-02-22 09:45:43 -0500

    create custom slide posts to use with main page

--- wp-content/themes/Bs-Design/functions.php
+++ wp-content/themes/Bs-Design/functions.php
@@ -398,22 +398,22 @@ add_shortcode('html5_shortcode_demo_2', 'html5_shortcode_demo_2'); // Place [htm
 // Create 1 Custom Post type for a Demo, called HTML5-Blank
 function create_post_type_html5()
 {
-    register_taxonomy_for_object_type('category', 'html5-blank'); // Register Taxonomies for Category
+    register_taxonomy_for_object_type('category', 'Main-Slides'); // Register Taxonomies for Category
     register_taxonomy_for_object_type('post_tag', 'html5-blank');
     register_post_type('html5-blank', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('HTML5 Blank Custom Post', 'html5blank'), // Rename these to suit
-            'singular_name' => __('HTML5 Blank Custom Post', 'html5blank'),
-            'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New HTML5 Blank Custom Post', 'html5blank'),
+            'name' => __('Main Slides', 'MainSlide'), // Rename these to suit
+            'singular_name' => __('Slide', 'slide'),
+            'add_new' => __('Add New', 'Slide'),
+            'add_new_item' => __('Add New Slide', 'html5blank'),
             'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit HTML5 Blank Custom Post', 'html5blank'),
-            'new_item' => __('New HTML5 Blank Custom Post', 'html5blank'),
-            'view' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'view_item' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'search_items' => __('Search HTML5 Blank Custom Post', 'html5blank'),
-            'not_found' => __('No HTML5 Blank Custom Posts found', 'html5blank'),
+            'edit_item' => __('Edit Slide', 'html5blank'),
+            'new_item' => __('New Slide', 'html5blank'),
+            'view' => __('View Slide', 'html5blank'),
+            'view_item' => __('View Slide', 'html5blank'),
+            'search_items' => __('Search Slide', 'html5blank'),
+            'not_found' => __('No Slides found', 'html5blank'),
             'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'html5blank')
         ),
         'public' => true,
