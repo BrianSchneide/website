abdd61d38c1d5f0e0fb694f61ce02a0ee4214338
Brian Schneide <brianschneide0@gmail.com>
2018-04-16 16:37:19 -0400

    reset env

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'web_portfolio');
+define('DB_NAME', 'brian-portfolio');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Cancel1note12!');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'O>K]&Iu-%.a/tqDvkwfY[;IAw/<?QzoCL#5,Bfm@E-8k9@[2_0^Lf`RAQ..#.nsK');
+define('SECURE_AUTH_KEY',  'n^I`@ZD[-t_P(#:=Zp9isuCs.3;8T?[B_h!`bN;4va3U%LdUBrZYI+&Cs+fN>s:]');
+define('LOGGED_IN_KEY',    '^y/S3zpOE]h&I$>[dn63vYAu*IRN6kX*[t-7)rs3s5mSRz*i5L%rwi{cAO<4h~8&');
+define('NONCE_KEY',        'A6{lN5QFfmX9EFEh5BqkPnS(yZ~Kum*3Tsj5..~G THS5!NStWsO,{7Nply/p4qY');
+define('AUTH_SALT',        '6+,: D2R..q7U`WACZLPc!b39![VZNG?ApHui9;BCe*t_@ya7G[L,mGjFsh(5/v(');
+define('SECURE_AUTH_SALT', ':LNn=Q>BGi^;rGItRfzDT_EQPP~h3ejmD-<6%IWR!ao:&0ar1(-dia&qL/b3mj0U');
+define('LOGGED_IN_SALT',   '`g_R6eJ}n5vF$$_(O5eeLpm53C)r!_7JC$z&#asCN<KtRxh>d!ti=dT!463ePUio');
+define('NONCE_SALT',       'tY 4mxU/Yj4QI$BI4;g_+JAC_260m--{MDQV|T%yBs4mu8vf;lA,TI&R?);F+.<3');
 
 /**#@-*/
 
@@ -77,11 +77,12 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
-/* disable wordpress admin panel editor */
+/* disable FTP authorization **/
 
-define( 'DISALLOW_FILE_EDIT', true );
+define('FS_METHOD', 'direct');
+
 
 /* That's all, stop editing! Happy blogging. */
 
